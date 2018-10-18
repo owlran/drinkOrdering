ed26ab3da35937a16daf636af4889c2d576cdbc4
owlran <owlran1088@gmail.com>
2018-10-19 02:26:57 +0800

    test: add e2e for adding the order

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -2,13 +2,27 @@
 // http://nightwatchjs.org/guide#usage
 
 module.exports = {
-  'default e2e tests': (browser) => {
+  'add drink order test': (browser) => {
     browser
       .url(process.env.VUE_DEV_SERVER_URL)
       .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
-      .end();
+      .click('.header__button')
+      .useXpath()
+      .setValue('//*[@id="app"]/div[3]/div/div[2]/div/div[2]/input[1]', 'Aaron Chen')
+      .setValue('//*[@id="app"]/div[3]/div/div[2]/div/div[2]/input[2]', '四季春')
+      .setValue('//*[@id="app"]/div[3]/div/div[2]/div/div[2]/input[3]', 30)
+      .click('//*[@id="app"]/div[3]/div/div[2]/div/div[3]/div[1]/div[2]')
+      .click('//*[@id="app"]/div[3]/div/div[2]/div/div[3]/div[2]/div[3]')
+      .setValue('//*[@id="app"]/div[3]/div/div[2]/div/div[4]/input', '我我我我我我我我我我我我我我我我我我我我')
+      .useCss()
+      .click('.modal__btn--confirm')
+      .waitForElementVisible('.orderList__card', 5000);
+
+      browser.expect.element('.order__drink').text.to.equal('四季春');
+      browser.expect.element('.order__name').text.to.equal('Aaron Chen');
+      browser.expect.element('.order__price').text.to.equal('$30');
+      browser.expect.element('.order__tag--sugar').text.to.equal('半糖');
+      browser.expect.element('.order__tag--ice').text.to.equal('少冰');
+      browser.end();
   },
 };
